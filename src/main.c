684bd2643d173f86af5e1eabc23992ab276dcd1a
Chip <chipz68q@gmail.com>
2018-11-29 21:50:12 +0500

    Implement hello gtk app

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
+#include <gtk/gtk.h>
 
-int main(void)
+#define		PROGRAM_TITLE	"Книжка оценок"
+
+int main(int argc, char *argv[])
 {
-	printf("Hello World!\n");
+	GtkWidget *window;
+
+    gtk_init (&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), PROGRAM_TITLE);
+
+	g_signal_connect(G_OBJECT(window), "destroy",
+						G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
 }

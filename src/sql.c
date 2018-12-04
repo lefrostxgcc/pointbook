ece83fab75c2a6196d6158a1e50fb1c2d8ad4e11
Chip <chipz68q@gmail.com>
2018-12-04 10:24:03 +0500

    Add sql_open implementation

--- src/sql.c
+++ src/sql.c
@@ -1,7 +1,18 @@
+#include <sqlite3.h>
 #include "sql.h"
 
 int		sql_open(const char *filename, void *connection)
 {
+	sqlite3		*db;
+	int			rc;
+
+	rc = sqlite3_open(filename, &db);
+	if (rc != SQLITE_OK)
+	{
+		sqlite3_close(db);
+		return 1;
+	}
+	connection = db;
 	return 0;
 }
 

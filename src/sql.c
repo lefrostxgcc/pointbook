93259d4a4f496cb9651b01914d7bba72d165188a
Chip <chipz68q@gmail.com>
2018-12-04 10:21:41 +0500

    Add base sql functions dummy

--- src/sql.c
+++ src/sql.c
@@ -1,1 +1,16 @@
 #include "sql.h"
+
+int		sql_open(const char *filename, void *connection)
+{
+	return 0;
+}
+
+int		sql_close(void *connection)
+{
+	return 0;
+}
+
+int		sql_exec(const char *query, sql_callback_t callback)
+{
+	return 0;
+}
--- src/sql.h
+++ src/sql.h
@@ -1,6 +1,10 @@
 #ifndef SQL_H
 #define SQL_H
 
+typedef int (*sql_callback_t)(void *, int, char **, char **);
 
+int		sql_open(const char *filename, void *connection);
+int		sql_close(void *connection);
+int		sql_exec(const char *query, sql_callback_t callback);
 
 #endif /* SQL_H */
